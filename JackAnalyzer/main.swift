74cad9e12978702f3a6187cf5241b269233c52a8
Murat Doğu <muratdogu@murats-macbook-pro.local>
2018-04-10 20:58:02 +0300

    Initial Commit

---
+++ JackAnalyzer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JackAnalyzer
+//
+//  Created by Murat Dogu on 10.04.2018.
+//  Copyright © 2018 Murat Dogu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
